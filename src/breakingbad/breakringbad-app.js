11e5a78d32af280b3d58f971d4044db8061a8ad0
Jose Ivan Campos Chavez <ivan820plus@hotmail.com>
2024-04-24 18:57:49 -0600

    quote added

--- src/breakingbad/breakringbad-app.js
+++ src/breakingbad/breakringbad-app.js
@@ -1,6 +1,43 @@
-export const BreakingbadApp = ( element ) => {
+/**
+ * @returns {Promise<Object>} quote information
+ */
+const fetchQuote = async() => {
     
-    document.querySelector('.title').innerHTML = 'Breaking Bad App'
-    element.innerHTML = 'Loading...'
+    const res = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+    const data = await res.json();
+
+    // console.log(data[0]);
+    return data[0];
+}
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const BreakingbadApp = async( element ) => {
+    document.querySelector('.title').innerHTML = 'Breakingbad App';
+    element.innerHTML = 'Loading...';
+
+    const quoteLabel = document.createElement('blockquote');
+    const authoLabel = document.createElement('h3');
+    const nextQuoteButton = document.createElement('button');
+    nextQuoteButton.innerText = 'Next Quote';
+
+
+    const renderQuote = ( {quote, author} ) => {
+        quoteLabel.innerHTML = quote;
+        authoLabel.innerHTML = author;
+        element.replaceChildren( quoteLabel, authoLabel, nextQuoteButton );
+    }
+
+    // Añadir listener
+    nextQuoteButton.addEventListener('click', async() => {
+        element.innerHTML = 'Loading...';
+        const data = await fetchQuote();
+        renderQuote(data);
+    })
     
+
+    fetchQuote()
+        .then( renderQuote );
 }
